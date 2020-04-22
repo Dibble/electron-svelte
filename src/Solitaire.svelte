6b7d57378ed42e302f57a5b1a3929089839547e8
Dibble <james.cotterell@newvoicemedia.com>
2020-04-22 15:16:14 +0100

    Can move cards from tableau to foundation

--- src/Solitaire.svelte
+++ src/Solitaire.svelte
@@ -49,18 +49,58 @@
     console.log('move waste to tab', card)
     for (let i = 0; i < tableau.length; i++) {
       let tableauCard = tableau[i][tableau[i].length - 1]
-      // can card be placed here
       if (card.value === 'king' && tableau[i].length === 0 || // king can move to empty column
         (values.indexOf(card.value) - values.indexOf(tableauCard.value) === 1) && // card value is suitable
         (suits.indexOf(card.suit) < 2 && suits.indexOf(tableauCard.suit) > 1 || // red card on black tableau
         suits.indexOf(card.suit) > 1 && suits.indexOf(tableauCard.suit) < 2)) { // black card on red tableau
           console.log('card can be placed', i)
           tableau[i] = [...tableau[i], {...card, face: 'up'}]
-          // tableau = tableau
           waste.pop()
           waste = waste
           break
       }
+    }
+  }
+
+  const moveFromTableauToFoundation = (column, row) => {
+    console.log('move from tableau to foundation', column, row)
+    if (tableau[column][row].face === 'down') { // can't move face down cards to foundation
+      console.log('card face down')
+      return
+    }
+
+    if (row >= tableau[column].length) { // not bottom card in the stack
+      console.log('not bottom card')
+      return
+    }
+
+    const card = tableau[column][row]
+    console.log('card', card)
+
+    let eligibleFoundationIndex = null
+    for (let i = 0; i < foundation.length; i++) {
+      const foundationColumn = foundation[i]
+      if (foundationColumn.length === 0) {
+        eligibleFoundationIndex = card.value === 'ace' ? i : null
+        break
+      }
+
+      if (foundationColumn[0].suit !== card.suit) continue
+
+      if (values.indexOf(foundationColumn[foundationColumn.length - 1].value) - values.indexOf(card.value) === 1) {
+        eligibleFoundationIndex = i
+        break
+      }
+    }
+    
+    console.log('eligible foundation index', eligibleFoundationIndex)
+    if (eligibleFoundationIndex !== null) {
+      console.log('moving card')
+      tableau[column].pop()
+      tableau[column][row - 1].face = 'up'
+      tableau = tableau
+
+      foundation[eligibleFoundationIndex] = [...foundation[eligibleFoundationIndex], card]
     }
   }
 
@@ -77,8 +117,9 @@
 <button on:click={dealFromStock}>{stock.length > 0 ? 'Deal from Stock' : 'Recycle'}</button>
 
 <h3>Tableau:</h3>
-{#each tableau as t}
+{#each tableau as t, col}
   <p>{t.length > 0 ? t.map(card => card.face === 'up' ? `${card.value} of ${card.suit}` : 'hidden').join(', ') : 'empty'}</p>
+  <button on:click={() => moveFromTableauToFoundation(col, t.length - 1)}>Move to Foundation</button>
 {/each}
 
 <button on:click={initialDeal}>Initial Deal</button>

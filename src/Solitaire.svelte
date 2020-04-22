fcc48823476242db9dafd185b13c5f28d0ffda92
Dibble <james.cotterell@newvoicemedia.com>
2020-04-22 12:21:01 +0100

    Deck is now shuffled on load

--- src/Solitaire.svelte
+++ src/Solitaire.svelte
@@ -3,24 +3,38 @@
   const values = ['ace', 'king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two']
   const deck = suits.map(suit => values.map(value => ({ face: 'down', suit, value}))).flat()
 
-  let foundation = [[], [], [], []]
-  let stock = [...deck]
-  let waste = []
-  let tableau = [[], [], [], [], [], [], []]
-
   const initialDeal = () => {
     for (let i = 0; i < tableau.length; i++) {
       let tableauColumn = i
-
+      
       while (tableauColumn < tableau.length) {
         const card = stock.pop()
         stock = stock
-  
+        
         tableau[tableauColumn] = [...tableau[tableauColumn], { ...card, face: tableauColumn === i ? 'up' : 'down' }]
         tableauColumn++
       }
     }
   }
+  
+  const shuffleCards = (inputCards) => {
+    const cards = [...inputCards]
+    const outputCards = []
+    
+    while(cards.length > 0) {
+      const indexToMove = Math.floor(Math.random() * cards.length)
+      
+      const [selectedCard] = cards.splice(indexToMove, 1)
+      outputCards.push(selectedCard)
+    }
+    console.log(outputCards)
+    return outputCards
+  }
+
+  let foundation = [[], [], [], []]
+  let stock = shuffleCards(deck)
+  let waste = []
+  let tableau = [[], [], [], [], [], [], []]
 </script>
 
 <p>Foundation: {foundation.map(f => f.length > 0 ? f[f.length - 1] : 'empty')}</p>
